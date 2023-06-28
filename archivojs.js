96cb5b6be8664cc3b4419728e66714fcdc04fef3
NeyenFrandino <Neyenfrandino1@gmail.com>
2023-06-27 20:27:09 -0500

    avanvamos con la calculadora comenzamos con el codigo js de vuelta, y pudimos con ella.

--- archivojs.js
+++ archivojs.js
@@ -5,11 +5,8 @@ function buscarPorId(elementoId){
 
 function confuguracionBotones(elementoid){
     let botonOperador = buscarPorId(elementoid);
-    console.log('id:', elementoid)
-    console.log('boton:', botonOperador)
     
     botonOperador.addEventListener('click', function(){
-        console.log('lolito')
         let cajaOperadores = buscarPorId('cajaDeOperadores');
         let valorBoton = botonOperador.textContent ;
         cajaOperadores.value = valorBoton;
@@ -17,9 +14,96 @@ function confuguracionBotones(elementoid){
     })
 }
 
-confuguracionBotones('botonSumar');
-confuguracionBotones('botonRestar');
-confuguracionBotones('botonMultiplicar');
-confuguracionBotones('botonDividir');
-confuguracionBotones('botonPotencia');
-confuguracionBotones('botonRaizCuadra');
+function suma (){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = numero1 + numero2;
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+
+}
+
+function resta (){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = numero1 - numero2;
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+
+}
+
+function multiplicar(){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = numero1 * numero2;
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+}
+
+function divicion(){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = numero1 / numero2;
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+}
+
+function potencia(){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = Math.pow(numero1, numero2);
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+}
+
+function raizCuadrada(){
+    let numero1 = +buscarPorId('cajaEntrada1').value;
+    let numero2 = +buscarPorId('cajaEntrada2').value;
+    let resultado = Math.sqrt(numero1);
+
+    let cajaResultado = buscarPorId('resultado');
+    cajaResultado.value = resultado
+    
+}
+
+function configurandoEventListener(idBotonReferencia, evento, funcion){
+    let botonDeReferenciaOperacion  = buscarPorId(idBotonReferencia);
+
+    botonDeReferenciaOperacion.addEventListener(evento, function(){
+        imprimirResultado(funcion)
+    })
+}
+
+
+function imprimirResultado(funcion) {
+    let botonIgual = buscarPorId('botonIgual');
+  
+    botonIgual.addEventListener('click', funcion);
+  }
+ 
+
+
+
+
+configurandoEventListener('botonSumar', 'click', suma);
+configurandoEventListener('botonRestar', 'click', resta);
+configurandoEventListener('botonMultiplicar', 'click', multiplicar);
+configurandoEventListener('botonDividir', 'click', divicion);
+configurandoEventListener('botonPotencia', 'click', potencia);
+configurandoEventListener('botonRaizCuadra', 'click', raizCuadrada);
+
+confuguracionBotones('botonSumar')
+confuguracionBotones('botonRestar')
+confuguracionBotones('botonMultiplicar')
+confuguracionBotones('botonDividir')
+confuguracionBotones('botonRaizCuadra')
+
+
+
+
+
